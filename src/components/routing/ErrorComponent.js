c409c3d6b38668efd54abcf05142eb973cee693b
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2022-09-28 17:34:55 +0530

    Delete src directory

--- src/components/routing/ErrorComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from '../../CSSSTYLE/error.module.css';
-function ErrorComponent() {
-  return (
-    <div className={classes.error}>ErrorComponent</div>
-  )
-}
-
-export default ErrorComponent
